9cde70c6d6758ff2fa8ddea9e0b81e3f20cbad06
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-08-04 08:56:48 +0100

    Popover issue messages instead of title text

--- src/components/IssueSelector.vue
+++ src/components/IssueSelector.vue
@@ -15,14 +15,28 @@ import {shell} from "electron";
         ref="issueKey"
       >
     </label>
-    <span
-      class="resolvedIssueTitle inputComponentPiece"
-      :title="issueTitle"
+    <popper
+      trigger="hover"
+      :visible-arrow="true"
+      :options="{
+        placement: 'bottom-start',
+        'boundaries-selector': 'v-application'
+      }"
+      :delay-on-mouse-over="300"
       v-if="issueKeyIsValid"
-      @click="focusOnIssueKey"
     >
-              {{ issueTitle }}
-            </span>
+      <div class="popper popper-issue-title">
+        {{ issueTitle }}
+      </div>
+      <span
+        class="resolvedIssueTitle inputComponentPiece"
+        v-if="issueKeyIsValid"
+        @click="focusOnIssueKey"
+        slot="reference"
+      >
+        {{ issueTitle }}
+      </span>
+    </popper>
     <button
       class="resolvedIssueLink"
       :title="`Open ${mountedIssueKey} in JIRA`"
@@ -36,6 +50,7 @@ import {shell} from "electron";
 
 <script lang="ts">
 import { Vue } from 'vue-property-decorator';
+import Popper from 'vue-popperjs';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -87,6 +102,7 @@ export default Vue.extend({
   },
   components: {
     FontAwesomeIcon,
+    Popper,
   },
   methods: {
     openUrl() {
@@ -128,4 +144,72 @@ export default Vue.extend({
   border-top-right-radius: 0;
   border-bottom-right-radius: 0;
 }
+</style>
+
+<style>
+  .popper-issue-title.popper {
+    width: auto;
+    background-color: #212121;
+    color: #fafafa;
+    text-align: center;
+    padding: 4px;
+    display: inline-block;
+    border-radius: 3px;
+    position: absolute;
+    font-weight: normal;
+    z-index: 200000;
+    font-size: 0.7rem;
+    max-width: 300px;
+  }
+  .popper-issue-title.popper .popper__arrow {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    position: absolute;
+    margin: 5px;
+  }
+  .popper-issue-title.popper[x-placement^="top"] {
+    margin-bottom: 5px;
+  }
+  .popper-issue-title.popper[x-placement^="top"] .popper__arrow {
+    border-width: 5px 5px 0 5px;
+    border-color: #212121 transparent transparent transparent;
+    bottom: -5px;
+    left: calc(50% - 5px);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .popper-issue-title.popper[x-placement^="bottom"] {
+    margin-top: 5px;
+  }
+  .popper-issue-title.popper[x-placement^="bottom"] .popper__arrow {
+    border-width: 0 5px 5px 5px;
+    border-color: transparent transparent #212121 transparent;
+    top: -5px;
+    left: calc(50% - 5px);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .popper-issue-title.popper[x-placement^="right"] {
+    margin-left: 5px;
+  }
+  .popper-issue-title.popper[x-placement^="right"] .popper__arrow {
+    border-width: 5px 5px 5px 0;
+    border-color: transparent #212121 transparent transparent;
+    left: -5px;
+    top: calc(50% - 5px);
+    margin-left: 0;
+    margin-right: 0;
+  }
+  .popper-issue-title.popper[x-placement^="left"] {
+    margin-right: 5px;
+  }
+  .popper-issue-title.popper[x-placement^="left"] .popper__arrow {
+    border-width: 5px 0 5px 5px;
+    border-color: transparent transparent transparent #212121;
+    right: -5px;
+    top: calc(50% - 5px);
+    margin-left: 0;
+    margin-right: 0;
+  }
 </style>
