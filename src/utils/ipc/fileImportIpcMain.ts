bd9c74ac919db5d15b81e09f0f8a1499fca03abe
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-08-21 17:39:08 +0100

    Linting fixes

--- src/utils/ipc/fileImportIpcMain.ts
+++ src/utils/ipc/fileImportIpcMain.ts
@@ -1,14 +1,14 @@
 import { PromiseIpcMain } from 'electron-promise-ipc/build/mainProcess';
 import { dialog } from 'electron';
-import BrowserWindow = Electron.BrowserWindow;
 import { promises } from 'fs';
+import BrowserWindow = Electron.BrowserWindow;
 
 export default function initialiseFileImportIpcMain(
   promiseIpcMain: PromiseIpcMain,
-  browserWindow: BrowserWindow
+  browserWindow: BrowserWindow,
 ): void {
-  promiseIpcMain.on('fileSelectAndRead', async (data: unknown): Promise<string|null> => {
-    const file = await dialog.showOpenDialog(browserWindow,{
+  promiseIpcMain.on('fileSelectAndRead', async (): Promise<string|null> => {
+    const file = await dialog.showOpenDialog(browserWindow, {
       properties: ['openFile'],
       filters: [
         { name: 'CSV', extensions: ['csv'] },
@@ -19,6 +19,6 @@ export default function initialiseFileImportIpcMain(
       return '';
     }
 
-    return await promises.readFile(file.filePaths[0], 'utf-8');
+    return promises.readFile(file.filePaths[0], 'utf-8');
   });
 }
